b47061eb19aa58987ea9283c73b2f56c5af13c56
Xoka74 <ya.vladimir565@mail.ru>
2022-12-01 19:56:39 +0500

    Initial commit

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace GameOfLife;
+
+public class Game
+{
+    public static bool[] GetCellNeighbours(bool[,] field, int x, int y)
+    {
+        var neighbours = new List<bool>();
+        for (var i = x - 1; i <= x + 1; i++)
+        for (var j = y - 1; j <= y + 1; j++)
+        {
+            try
+            {
+                neighbours.Add(field[i, j]);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        return neighbours.ToArray();
+    }
+
+    public static int GetAliveNeighbours(bool[] neighbours)
+    {
+        var count = 0;
+        foreach (var neighbour in neighbours)
+            if (neighbour)
+                count++;
+
+        return count;
+    }
+
+    public static void TransformCell(bool[,] field, int x, int y)
+    {
+        var neighbours = GetCellNeighbours(field, x, y);
+        var aliveNeighbours = GetAliveNeighbours(neighbours);
+        switch (aliveNeighbours)
+        {
+            case 3:
+                field[x, y] = true;
+                break;
+            case 2: break;
+            default:
+                field[x, y] = false;
+                break;
+        }
+    }
+
+    public static bool[,] NextStep(bool[,] field)
+    {
+        var height = field.GetLength(0);
+        var width = field.GetLength(1);
+        for (var x = 0; x < width; x++)
+        for (var y = 0; y < height; y++)
+            TransformCell(field, x, y);
+        return field;
+    }
+}
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GameOfLife;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var field = new bool[20, 20];
+        while (true)
+        {
+            Paint(field);
+            Thread.Sleep(500);
+            field = Game.NextStep(field);
+        }
+    }
+    private static void Paint(bool[,] field)
+    {
+        Console.SetCursorPosition(0, 0);
+        for (int y = 0; y < field.GetLength(1); y++)
+        {
+            for (int x = 0; x < field.GetLength(0); x++)
+            {
+                var symbol = field[x, y] ? '#' : ' ';
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
